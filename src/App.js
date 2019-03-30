ccefc9cff482474a3978e32059a371be55828d14
Alex Joo <alexjoo93@gmail.com>
2019-03-30 18:55:28 -0400

    form and api complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Titles from './components/titles';
+import Form from './components/form';
+import Weather from './components/weather';
+
+const Api_Key = "d88641e244ebfe3675074c6471e3e021";
 
 class App extends Component {
+
+  state ={
+    temperature: undefined,
+    city: undefined, 
+    country: undefined, 
+    humidity: undefined, 
+    description: undefined, 
+    error: undefined
+  }
+  
+  getWeather = async (e) => {
+    e.preventDefault(); //passing event argument and preventing full page refresh when api is called on
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const response = await api_call.json(); //take in result of api call and convert to json
+    console.log(response);
+
+    if (city && country) {
+    this.setState({
+      temperature: response.main.temp,
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity, 
+      description: response.weather[0].description,
+      error: ""
+
+    })
+  }
+  else {
+    this.setState({
+      error:"Please enter the values.."
+    })
+  }
+}
+
+resetForm = () => {
+  document.getElementById("weather-form").reset();
+}
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Titles />
+        <Form loadWeather={this.getWeather}/>
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/titles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titles = (prop)=>  {
+    return (
+        <div> 
+            <h1> Weather App </h1>
+            <p>Search weather in different cities...</p>
+        </div>
+    )
+}
+
+export default Titles; 
